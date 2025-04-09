91f057342aaea2a6d49077fc3855500eba31efb8
0xLPircy <taveesha.agarwal@gmail.com>
2025-04-09 16:27:53 +0530

    dark sec resp

--- src/components/sections/LandingDark.tsx
+++ src/components/sections/LandingDark.tsx
@@ -1,18 +1,20 @@
 const LandingDark = () => {
   return (
-    <section className="bg-[#4E615F] text-[#EEEBE2] h-[93vh] py-20 w-full">
-      <div className="flex flex-col gap-[0.6rem] text-left pl-30">
-        <h2 className="quattrocento-bold text-[2.7rem] max-w-[600px]">Experience India's first platonic dating app</h2>
-        <p className="font-karla text-[1.2rem]">Apply to our Beta test program. Get Early Access</p>
-        <h1 className="quattrocento-bold text-[2.7rem]">@ 40% off.</h1>
-        <button className="w-fit bg-[#EEEBE2] px-[1rem] py-[6px] text-[#1F2726]">
+    <section className="bg-[#4E615F] text-[#EEEBE2] h-[90%] py-10 w-full max-w-[1800px]">
+      <div className="flex flex-col lg:gap-[0.6rem] gap-[0.3rem] text-left lg:pl-30 md:pl-20 sm:pl-10 pl-6 lg:mt-10 mt-6 lg:mb-[-12rem] sm:mb-[-9rem] mb-[-3rem]">
+        <h2 className="quattrocento-bold lg:text-[2.7rem] lg:leading-[3rem] sm:text-[2.1rem] sm:leading-[2.4rem] text-[1.8rem] leading-[2.1rem]">Experience India's first <br/>platonic dating app</h2>
+        <p className="font-karla lg:text-[1.2rem] sm:text-[1.05rem] text-[0.9rem]">Apply to our Beta test program. Get Early Access</p>
+        <h1 className="quattrocento-bold lg:text-[2.7rem] lg:leading-[3rem] sm:text-[2.25rem] sm:leading-[2.4rem] text-[1.8rem] leading-[2.1rem]">@ 40% off.</h1>
+        <button className="w-fit bg-[#EEEBE2] md:px-[1.8rem] md:py-[12px] px-[1.5rem] py-[9px] text-[#1F2726] md:text-[1rem] text-[0.9rem]
+        hover:bg-[#EEEBE2d1] hover:px-[1.5rem] hover:tracking-[0.5px]
+            hover:cursor-pointer">
           Join Waitlist
         </button>
       </div>
-      <div className="flex flex-col mt-[-33rem]">
-        <img src="/assets/RHS.svg" alt="" className="mb-[-4.5rem] w-[48%] place-self-end z-20"/>
-        <img src="/assets/mid.svg" alt="" className="mr-[-12rem] w-[39%] place-self-center mb-[-12rem] mt-[-15rem] z-10"/>
-        <img src="/assets/LHS.svg" alt="" className="mt-[-4.5rem] w-[50%] place-self-start z-20"/>
+      <div className="flex flex-col w-full xl:mt-[-33rem] lg:mt-[-21rem] md:mt-[-18rem] sm:mt-[-9rem] mt-[-3rem]">
+        <img src="/assets/RHS.svg" alt="" className="rtlAnim xl:mb-[-4.5rem] md:mb-[-4.2rem] sm:mb-[-2.4rem] mb-[-1.5rem] w-[48%] place-self-end z-20"/>
+        <img src="/assets/mid.svg" alt="" className="fadeIn xl:mr-[-12rem] md:mr-[-9rem] mr-[-3rem] w-[39%] place-self-center xl:mb-[-12rem] xl:mt-[-15rem] md:mb-[-6rem] lg:mt-[-13rem] md:mt-[-9rem] mb-[-3.6rem] sm:mt-[-6.6rem] mt-[-3.6rem] z-10"/>
+        <img src="/assets/LHS.svg" alt="" className="ltrAnim xl:mt-[-4.5rem] md:mt-[-2.1rem] mt-[-1.2rem] w-[50%] place-self-start z-20"/>
       </div>
     </section>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ const Home = () => {
         <Hero/>
         <AboutUsLanding/>
         <Features/>
-        {/* <LandingDark/> */}
+        <LandingDark/>
         {/* <ContactUs/> */}
         <Footer/>
     </main>
