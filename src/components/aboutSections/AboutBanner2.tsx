af6d939201adcb7a43d23f3914b73bd5dce82653
0xLPircy <taveesha.agarwal@gmail.com>
2025-04-09 19:32:39 +0530

    perfect resp

---
+++ src/components/aboutSections/AboutBanner2.tsx
@@ -0,0 +1,8 @@
+const AboutBanner2 = () => {
+    return (
+        <section className="flex flex-col justify-center items-center lg:px-30 md:px-20 py-10  w-full bg-[#4E615F] text-[#EEEBE2] quattrocento-regular md:text-[1.8rem] md:leading-[2.1rem] text-[0.9rem] leading-[1.2rem]">
+            <h1 className="max-w-[1800px] text-center">People often lose friends in the competitive pursuit of their careers. And we may find colleagues along the way, but not friends.</h1>
+        </section>
+    )
+}
+export default AboutBanner2
